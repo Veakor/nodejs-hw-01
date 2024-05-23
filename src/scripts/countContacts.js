9e622eb47ec92bb4b4cd1f42f28bc6283aa683c3
Veakor <135655247+Veakor@users.noreply.github.com>
2024-05-23 08:50:24 +0200

    add

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,6 +1,46 @@
 import fs from 'fs/promises';
 import { PATH_DB } from '../constants/constants.js';
 
+// Функція для підрахунку контактів
+async function countContacts() {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+    const count = contacts.length;
+    console.log(count);
+  } catch (error) {
+    console.error('Error reading contacts:', error);
+  }
+}
+
+countContacts();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import fs from 'fs/promises';
+import { PATH_DB } from '../constants/constants.js';
+
 export const countContacts = async () => {
   try {
     const data = await fs.readFile(PATH_DB);
@@ -10,4 +50,4 @@ export const countContacts = async () => {
   }
 };
 
-console.log(countContacts());
+console.log(countContacts());*/
